f66b6fbd4cd32ee1408309a4b836fe94a4bb8c5e
91907 <91907@DESKTOP-0C1G30L>
2026-04-24 12:52:54 +0530

    Updated and comitted

---
+++ src/test/java/Demo/FileUtility.java
@@ -0,0 +1,11 @@
+package Demo;
+
+public class FileUtility {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Updated repository");
+
+	}
+
+}
--- src/test/java/Demo/sampleTest.java
+++ src/test/java/Demo/sampleTest.java
@@ -3,7 +3,8 @@ package Demo;
 public class sampleTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("Hi this is my First commit");
 
 	}
 
